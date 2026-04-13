231cdae92f13b304544f7779a8b055ca1585b8c6
“Gpeter01” <240795083+Gpeter01@users.noreply.github.com>
2026-04-13 13:42:13 +0100

    Create the playRound() function

--- script.js
+++ script.js
@@ -40,3 +40,96 @@ function getComputerChoice() {
         return;
     }
 }
+function playRound(a, b) {
+    if (a == choiceOne && b == choiceTwo) {
+        computerScore++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+            
+            You lose! Paper beats Rock`);
+        alert(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+            
+            You lose! Paper beats Rock`);
+    } else if (a == choiceTwo && b == choiceOne) {
+        humanScore++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer;s Choice: ${b}
+
+            You win! Paper beats Rock`);
+        alert(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer;s Choice: ${b}
+
+            You win! Paper beats Rock`);
+    }
+    if (a == choiceOne && b == choiceThree) {
+        humanScore++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You win! Rock beats Scissors`);
+        alert(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You win! Rock beats Scissors`);
+    } else if (a == choiceThree && b == choiceOne) {
+        computerScore++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You lose! Rock beats Scissors`);
+        alert(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You lose! Rock beats Scissors`);
+    }
+    if (userChoice == choiceTwo && b == choiceThree) {
+        computerScore++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You lose! Scissors beats Paper`);
+        alert(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You lose! Scissors beats Paper`);
+    } else if (a == choiceThree && b == choiceTwo) {
+        humanScore++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+
+            You win! Scissors beats Paper`);
+        alert(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+            You win! Scissors beats Paper`);
+    } else if (userChoice == b) {
+        drawGame++;
+        console.log(`Round ${i} / 5 
+            Your Choice: ${a}
+            Computer's Choice: ${b}
+            
+            
+            Draw!!`);
+        alert(`Your Choice: ${a}
+            Computer's Choice: ${b}
+            
+            
+            Draw!!`);
+    }
+    if (a != undefined || null) {
+        console.log("");
+        console.log("");
+    }
+}
