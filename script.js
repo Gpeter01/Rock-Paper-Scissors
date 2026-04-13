6a932f539f8cfd24b7425c9901db59209da23c53
“Gpeter01” <240795083+Gpeter01@users.noreply.github.com>
2026-04-13 13:04:52 +0100

    Create the getComputerChoice() function

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ let humanScore = 0;
 let drawGame = 0;
 let userChoice;
 let computerChoice;
+
+function getComputerChoice() {
+    if (computerProb < 33) {
+        computerChoice = choiceOne;
+    }
+    if (computerProb >= 33 && computerProb < 66) {
+        computerChoice = choiceTwo;
+    }
+    if (computerProb >= 66 && computerProb < 100) {
+        computerChoice = choiceThree;
+    }
+}
